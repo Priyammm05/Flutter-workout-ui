77e59d05587415ab6cb3396114a61b9fffd07593
Priyam <priyam05soni@gmail.com>
2022-01-29 10:27:53 +0530

    models used in project

---
+++ lib/models/upperBodyWorkout.dart
@@ -0,0 +1,79 @@
+// ignore_for_file: file_names
+
+class UpperBodyWorkout {
+  final String imagePath, name, instruction;
+
+  UpperBodyWorkout(
+      {required this.imagePath, required this.name, required this.instruction});
+}
+
+final upperBody = [
+  [
+    UpperBodyWorkout(
+      imagePath: "images/chest.png",
+      name: "Bench press",
+      instruction: "3 sets - 6 repitions",
+    ),
+    UpperBodyWorkout(
+      imagePath: "images/chest.png",
+      name: "Dumbell press",
+      instruction: "4 sets - 6 repitions",
+    ),
+    UpperBodyWorkout(
+      imagePath: "images/chest.png",
+      name: "Dips",
+      instruction: "2 sets - 10 repitions",
+    ),
+  ],
+  [
+    UpperBodyWorkout(
+      imagePath: "images/back.png",
+      name: "Pull ups",
+      instruction: "2 sets - 8 repitions",
+    ),
+    UpperBodyWorkout(
+      imagePath: "images/back.png",
+      name: "Deadlift",
+      instruction: "2 sets - 4 repitions",
+    ),
+    UpperBodyWorkout(
+      imagePath: "images/back.png",
+      name: "Lat pulldown",
+      instruction: "4 sets - 6 repitions",
+    ),
+  ],
+  [
+    UpperBodyWorkout(
+      imagePath: "images/chest.png",
+      name: "Bench press",
+      instruction: "3 sets - 6 repitions",
+    ),
+    UpperBodyWorkout(
+      imagePath: "images/chest.png",
+      name: "Dumbell press",
+      instruction: "4 sets - 6 repitions",
+    ),
+    UpperBodyWorkout(
+      imagePath: "images/chest.png",
+      name: "Dips",
+      instruction: "2 sets - 10 repitions",
+    ),
+  ],
+  [
+    UpperBodyWorkout(
+      imagePath: "images/back.png",
+      name: "Pull ups",
+      instruction: "2 sets - 8 repitions",
+    ),
+    UpperBodyWorkout(
+      imagePath: "images/back.png",
+      name: "Deadlift",
+      instruction: "2 sets - 4 repitions",
+    ),
+    UpperBodyWorkout(
+      imagePath: "images/back.png",
+      name: "Lat pulldown",
+      instruction: "4 sets - 6 repitions",
+    ),
+  ],
+];
