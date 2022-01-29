9d74c9e8d8a7d5b1d81576b2c3d04a7801e9c92d
Priyam <priyam05soni@gmail.com>
2022-01-29 10:29:55 +0530

    main file

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:diet_app_ui/pages/profile.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(
+      SystemUiOverlayStyle(statusBarColor: Colors.transparent),
+    );
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Profile(),
+    );
+  }
+}
